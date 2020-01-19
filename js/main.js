63afd1a256d4e2b5f8a0d978d1a5cf625b176f5f
MilosLjuboja <milos.ljuboja.279.18@ict.edu.rs>
2020-01-19 16:20:38 +0100

    izmena2

--- js/main.js
+++ js/main.js
@@ -10,9 +10,24 @@ window.onload=function(){
     }
     meni+="</ul>"
     document.getElementById("nav").innerHTML=meni;
+    
+    var nizMeni=["Početna","Usluge","O nama","Lokacija","Kontakt"];
+    var linkoviMeni=["#","#","#","#","#"];
+    var meni="";
+    meni+="<ul id='listaHamburger' class='nevidljiv'>"
+    for(var i=0;i<nizMeni.length;i++){
+        meni+=`<li class="tekstMeni"><a href=${linkoviMeni[i]}>${nizMeni[i]}</a></li>`
+    }
+    meni+="</ul>"
+    document.getElementById("hamburgerPodMeni").innerHTML+=meni;
     //KRAJ MENIJA
     //SLAJDER
     
     
 
 }
+$(document).ready(function(){
+    $('#hamburger').click(function(){
+        $('#hamburgerPodMeni').find('ul').slideToggle('fast');
+    });
+})
