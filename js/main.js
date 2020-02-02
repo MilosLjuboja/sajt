9653fa898fd3c63d2a84c5ec761f2f5f2effc581
MilosLjuboja <milos.ljuboja.279.18@ict.edu.rs>
2020-02-02 21:23:02 +0100

    promena

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 window.onload=function(){
-    //DINAMICKI MENI
-    var nizMeni=["Početna","Projekti","O nama","Lokacija","Kontakt","Autor"];
+    //DINAMICKA NAVIGACIJA
+    var nizMeni=["Početna","Projekti","O nama","Kontakt","Autor"];
     var linkoviMeni=["#","#projekti","#","#","#","#"];
     var meni="";
     meni+="<ul id='lista'>"
@@ -10,8 +10,8 @@ window.onload=function(){
     }
     meni+="</ul>"
     document.getElementById("nav").innerHTML=meni;
-    
-    var nizMeni=["Početna","Projekti","O nama","Lokacija","Kontakt","Autor"];
+    //
+    var nizMeni=["Početna","Projekti","O nama","Kontakt","Autor"];
     var linkoviMeni=["#","#projekti","#","#","#"];
     var meni="";
     meni+="<ul id='listaHamburger' class='nevidljiv'>"
@@ -20,7 +20,7 @@ window.onload=function(){
     }
     meni+="</ul>"
     document.getElementById("hamburgerPodMeni").innerHTML+=meni;
-    //KRAJ MENIJA
+    //KRAJ NAVIGACIJE
     //SLAJDER, POCINJE OD 34 LINIJE A ZAVRSAVA SE U 49
     slajder()
 }
@@ -47,5 +47,17 @@ function slajder(){
     }
     setTimeout("slajder()",3000)
 }
-//SLAJDER
+var divProjekti=document.getElementById('projekti')
+var tekst="<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p><p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>"
+divProjekti.innerHTML+=tekst
+//SLAJDER KRAJ
+//SLIKE PROJEKATA
+var projektiSlike=document.getElementById("projektiSlike")
+var nizSlika=["img/slikaProjekti1.jpg","img/slikaProjekti1.jpg","img/slikaProjekti1.jpg","img/slikaProjekti1.jpg"]
+var nizNaslova=["Los Angeles","New York","Los Angeles","New York"]
+var blokovi=""
+for(var i=0;i<=nizSlika.length-1;i++){
+blokovi+=`<div class="projekti col-lg-6"><h2>${nizNaslova[i]}</h2><img src="${nizSlika[i]}" alt="${nizNaslova[i]}" /></div>`
+}
+projektiSlike.innerHTML=blokovi
 
